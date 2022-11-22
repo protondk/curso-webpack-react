34f9894db40142abfb1a76761b15dd08ea5d16c4
protondk <edgarjosue2911@gmail.com>
2022-11-22 14:02:49 -0600

    React Project ready for deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
+//Do not use {ReactDOM}. Leave it without brackets.
+import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
+import "./styles/global.scss";
 
 ReactDOM.render(<App />, document.getElementById("app"));
